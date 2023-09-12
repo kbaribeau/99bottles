7fc602371d92bedf1d6403613a9fbf30f3ea5c16
Kevin Baribeau <kevin.baribeau@gmail.com>
2023-09-12 11:26:45 -0600

    Final result

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,30 @@
 class BottlesSong
+  VERSES = {
+    0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+    1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+    2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
+  }
+
+  def sing
+    verses(99, 0)
+  end
+
+  def verses(high_count, low_count)
+    (low_count..high_count).map do |bottle_count|
+      verse(bottle_count)
+    end.reverse.concat(['']).join("\n")
+  end
+
   def verse(bottle_count)
-    if bottle_count == 1
-      <<~END
-        1 bottle of beer on the wall, 1 bottle of beer.
-        Take it down and pass it around, no more bottles of beer on the wall.
-      END
-    else
-      <<~END
-        #{bottle_count} bottles of beer on the wall, #{bottle_count} bottles of beer.
-        Take one down and pass it around, #{bottle_count - 1} bottles of beer on the wall.
-      END
-    end
+    VERSES.fetch(bottle_count, generic_verse(bottle_count))
+  end
+
+  private
+
+  def generic_verse(bottle_count)
+    <<~END
+      #{bottle_count} bottles of beer on the wall, #{bottle_count} bottles of beer.
+      Take one down and pass it around, #{bottle_count - 1} bottles of beer on the wall.
+    END
   end
 end
